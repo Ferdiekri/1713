f099b473655ac60c4205a19b314a387e77f67643
Eder Ibáñez Rojo <equ_406@hotmail.com>
2019-06-14 10:12:32 +0200

    Cambiado nombre del usuario que sube los cambios.

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("Hola Karakola. ¿Qué pasa tio?");
+		System.out.println("He cambiado las credenciales de usuario.");
 
 	}
 
