2cc640d85ee6a90b41496e01383b3a814fd91e9c
Eder Ibáñez Rojo <equ_406@hotmail.com>
2019-06-21 12:43:20 +0200

    MAVENIZADO el proyecto.

---
+++ src/main/java/PruebasCalificadores.java
@@ -0,0 +1,13 @@
+import com.ipartek.formacion.Perro;
+
+public class PruebasCalificadores {
+
+	public static void main(String[] args) {
+		Perro p = new Perro();
+		
+		p.chip="123-ABCdef";
+		//p.raza = "Boxer";
+
+	}
+
+}
